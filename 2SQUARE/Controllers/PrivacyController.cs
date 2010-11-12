8d27463aa96ddcbf1c3222c5c67bded54ee63296
Alan Lai <anlai@ucdavis.edu>
2010-11-12 14:05:09 -0800

    added view stubs for all steps for security and privacy

--- 2SQUARE/Controllers/PrivacyController.cs
+++ 2SQUARE/Controllers/PrivacyController.cs
@@ -8,11 +8,66 @@ namespace _2SQUARE.Controllers
 {
     public class PrivacyController : Controller
     {
-        //
-        // GET: /Privacy/
-
-        public ActionResult Index()
+        public ActionResult Step1()
         {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step2()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step3()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step4()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step5()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step6()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step7()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step8()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step9()
+        {
+            throw new NotImplementedException();
+
             return View();
         }
 
--- 2SQUARE/Controllers/SecurityController.cs
+++ 2SQUARE/Controllers/SecurityController.cs
@@ -6,13 +6,68 @@ using System.Web.Mvc;
 
 namespace _2SQUARE.Controllers
 {
-    public class SecurityController : Controller
+    public class SecurityController : SuperController
     {
-        //
-        // GET: /Security/
-
-        public ActionResult Index()
+        public ActionResult Step1()
         {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step2()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step3()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step4()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step5()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step6()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step7()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step8()
+        {
+            throw new NotImplementedException();
+
+            return View();
+        }
+
+        public ActionResult Step9()
+        {
+            throw new NotImplementedException();
+
             return View();
         }
 
