10fb1468393a6290dbb9a5aaeaaff4c5edd581a3
Alan Lai <anlai@ucdavis.edu>
2010-11-12 14:00:19 -0800

    added privacy and security controllers

---
+++ 2SQUARE/Controllers/SecurityController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2SQUARE.Controllers
+{
+    public class SecurityController : Controller
+    {
+        //
+        // GET: /Security/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
