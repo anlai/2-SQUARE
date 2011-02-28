4f835d3f003cccb0abd5bd0fd80a623765306cb9
Alan Lai <anlai@ucdavis.edu>
2011-02-27 19:33:45 -0800

    added link generator to automatically generate proper redirection links to the main page for steps.

---
+++ 2SQUARE/Helpers/LinkGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using _2SQUARE.Models;
+
+namespace _2SQUARE.Helpers
+{
+    /// <summary>
+    /// Generates links for the primary pages for the steps
+    /// </summary>
+    public class LinkGenerator
+    {
+        public static string CreateString(RequestContext requestContext, ProjectStep projectStep)
+        {
+            var step = projectStep.Step;
+
+            var urlHelper = new UrlHelper(requestContext);
+            return urlHelper.Action(step.Action, step.Controller,
+                                         new {id = projectStep.Id, projectId = projectStep.ProjectId});
+        }
+
+        public static RedirectResult CreateRedirectResult(RequestContext requestContext, ProjectStep projectStep)
+        {
+            return new RedirectResult(CreateString(requestContext, projectStep));
+        }
+    }
+}
